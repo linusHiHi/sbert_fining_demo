1dc0d9d0eb4cfa0e1bfa5350bbdfd18555ef75ed
mia <cosmolatina@163.com>
2024-12-22 18:56:27 +0800

    fix

--- ttest_output.py
+++ ttest_output.py
@@ -32,24 +32,26 @@ for sheet_name, df in data_excel.items():
 
 
 # 测试新的输入文本
-input_texts = ["千问","阿里巴巴"]
+input_texts = ["千问","阿里巴巴", "天人合一", "方外之人", "独钟", "逆旅", "风华正茂", "栖息地", "魑魅魍魉",
+               "白璧微瑕", "庙堂之高", "鹤立鸡群", "潦倒", "梦魇", "炉火纯青", "五光十色", "云销雨霁", "心有灵犀",
+               "草木皆兵", "涓滴", "鬼斧神工", "风月无边"]
+
 embeddings = model.encode(input_texts)
 
 # 假设候选文本和它们的嵌入已经准备好
-#candidate_texts = data
-candidate_texts = ["汉堡","哈布斯堡","谢林", "引力波","火车票","冰淇淋","果冻","面包"]
+candidate_texts = data
+#candidate_texts = ["汉堡","哈布斯堡","谢林", "引力波","火车票","冰淇淋","果冻","面包"]
 candidate_embeddings = model.encode(candidate_texts)
 
 # 计算余弦相似度
-similarities = [cosine_similarity(embeddings, candidate_embedding) for candidate_embedding in candidate_embeddings]
+similarities = cosine_similarity(embeddings, candidate_embeddings)
 similarities=np.array(similarities)
-similarities=np.vstack(similarities).reshape(-1)
+similarities=np.vstack(similarities).reshape(len(input_texts), len(candidate_texts))
 # 输出前{top_n}个最相似的文本
 top_n = 5
-top_indices = similarities.argsort()[0:top_n]
-last = similarities.argsort()[-top_n:-1]
-print(f"original: {input_texts}")
-for idx in top_indices:
-    print(f"Text: {candidate_texts[idx]}, Similarity Score: {similarities[idx]}")
-for idx in last:
-    print(f"fail Text: {candidate_texts[idx]}, Similarity Score: {similarities[idx]}")
+for i in range(len(input_texts)):
+    top_indices = similarities.argsort()[i][-top_n:]
+    if similarities[top_indices].sum() > 0:
+        print(f"original: {input_texts}")
+    for idx in top_indices:
+        print(f"Text: {candidate_texts[idx]}, Similarity Score: {similarities[i][idx]}")
