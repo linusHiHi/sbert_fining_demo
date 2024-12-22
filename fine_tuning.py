54fee1acdf20ea184f27008d5b41bbbdf4a66d3e
mia <cosmolatina@163.com>
2024-12-22 16:55:16 +0800

    hot fix

--- fine_tuning.py
+++ fine_tuning.py
@@ -64,7 +64,7 @@ def eval_metrics(model_, loader):
             corrcoef_sum += compute_corrcoef(cosine_score.detach().cpu().numpy(), labels.detach().cpu().numpy())
             count += 1
     return loss_sum / count, corrcoef_sum / count
-
+"""
 # 数据加载
 data_excel = pd.read_excel("./data/data.xlsx", sheet_name=None)
 data = convert_excel_to_classification_format(data_excel)
@@ -73,7 +73,8 @@ data = [
     ["打不开花呗", "为什么花呗打不开", 1],
     ["花呗收钱就是用支付宝帐号收嘛", "我用手机花呗收钱", 0],
     ["花呗买东西，商家不发货怎么退款", "花呗已经分期的商品 退款怎么办", 0]
-]"""
+]
+
 train_loader = DataLoader(ATECDataset(data), batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 val_loader = DataLoader(ATECDataset(data), batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
 
