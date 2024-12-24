5939aa5f4ec890c01fd49394412923a0fcc01994
mia <cosmolatina@163.com>
2024-12-24 16:52:49 +0800

    refactored

--- fine_tune_with_sbert_api.py
+++ fine_tune_with_sbert_api.py
@@ -1,22 +1,21 @@
-
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
-from torch.utils.data import DataLoader
-
-from sentence_transformers import SentenceTransformer, LoggingHandler, losses, models
-
 import logging
-
 import pandas as pd
 import math
 
+from torch.utils.data import DataLoader
+
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers import SentenceTransformer, LoggingHandler, losses, models
+
 from util import split_data, convert_util, sampling
+
 """
-******************************************************
+************************ config ******************************
 """
 model_name = 'bert-base-chinese'
 model_save_path = 'sbert_result'
-path_to_whole_dataset = "data/full_data_with_all_new_sentence.csv"
-path_to_raw_excel_dataset = "data/source_data.xlsx"
+path_to_dataset = "data/sampled_data_with_all_new_sentence.csv"
+# path_to_raw_excel_dataset = "data/source_data.xlsx"
 train_batch_size = 16
 num_epochs = 4
 warm_up_rate = 0.1
@@ -32,7 +31,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
   handlers=[LoggingHandler()])
 
 """
-*******************Model defining**************************
+******************* Model defining **************************
 """
 
 # Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
@@ -47,20 +46,16 @@ pooling_model = models.Pooling(
 )
 
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-
-
-"""
-******************data dealing**************************
-"""
+train_loss = losses.CosineSimilarityLoss(model=model)
 
 """
-data_excel = pd.read_excel(path_to_raw_excel_dataset, sheet_name=None)
-data = convert_excel_to_classification_format(data_excel,"excel")
+****************** data dealing **************************
 """
-data_csv = pd.read_csv(path_to_whole_dataset)
+
+data_csv = pd.read_csv(path_to_dataset)
 data = convert_util(data_csv,"csv",classes)
 
-"""
+"""以上两行可用性存疑,以下为替换代码
 train_samples, test_samples, dev_samples = split_data(data)
 
 train_samples = sampling(train_samples)
@@ -68,11 +63,12 @@ dev_samples = sampling(dev_samples)
 test_samples = sampling(test_samples)
 """
 
+#分训练集
 train_samples, test_samples, dev_samples = split_data(data).map(sampling)
 
 train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
-train_loss = losses.CosineSimilarityLoss(model=model)
 evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name=evaluate_name)
+
 warmup_steps = math.ceil(len(train_dataloader) * num_epochs * warm_up_rate) #10% of train data for warm-up
 
 """
@@ -86,7 +82,7 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
   output_path=model_save_path)
 
 """
-*****************  test  ******************
+*****************  test saved model ******************
 """
 
 model = SentenceTransformer(model_save_path)
